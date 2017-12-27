b35eabac137eef35944fe5be6493d5fdf6b3f981
Dan Ward <daniel.ward@forrent.com>
2017-12-27 17:21:22 -0500

    Add eslint, add contests type, add type to me type

--- database/pgdb.js
+++ database/pgdb.js
@@ -9,6 +9,15 @@ module.exports = pgPool => {
       `, [apiKey]).then(response => {
         return humps.camelizeKeys(response.rows[0]);
       })
+    },
+
+    getContests(user) {
+      return pgPool.query(`
+        select * from contests
+        where created_by = $1
+      `, [user.id]).then(response => {
+        return humps.camelizeKeys(response.rows)
+      });
     }
   }
 }
--- types/me.js
+++ types/me.js
@@ -2,20 +2,30 @@ const {
   GraphQLObjectType,
   GraphQLNonNull,
   GraphQLString,
+  GraphQLList,
   GraphQLID
-} = require('graphql');
+} = require('graphql')
+
+const pgdb = require('../database/pgdb');
+const ContestType = require('./contest');
 
 module.exports = new GraphQLObjectType({
   name: 'MeType',
   fields: {
     id: { type: GraphQLID },
-    email: { type: new GraphQLNonNull(GraphQLString)},
+    email: { type: new GraphQLNonNull(GraphQLString) },
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString },
     fullName: {
       type: GraphQLString,
-      resolve: objj => `${obj.firstName} ${obj.lastName}`
+      resolve: obj => `${obj.firstName} ${obj.lastName}`
     },
-    createdAt: { type: GraphQLString }
+    createdAt: { type: GraphQLString },
+    contests: {
+      type: new GraphQLList(ContestType),
+      resolve: (obj, args, { pgPool }) => {
+        return pgdb(pgPool).getContests(obj);
+      }
+    }
   }
 });
